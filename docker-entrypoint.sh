39068b4625325e1ea91d80b2463deb96b9341290
Shie1 <sonkolyw@gmail.com>
2023-11-17 21:25:56 +0100

    fix?

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,8 +7,6 @@ export DBUS_SESSION_BUS_ADDRESS=/dev/null
 
 echo "Starting X virtual framebuffer (Xvfb) in background..."
 
-pkill "chrome" &
-
 Xvfb -ac :99 -screen 0 1280x1024x16 > /dev/null 2>&1 &
 
 exec "$@"
